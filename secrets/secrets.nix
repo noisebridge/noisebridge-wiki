3d2e68f1ea5185aef755d76c3914553225961e5a
Jet <jet@extremist.software>
2026-03-26 01:44:24 -0700

    chore(secrets): add age recipients and secret manifest

---
+++ secrets/secrets.nix
@@ -0,0 +1,22 @@
+let
+  readRecipient = path: builtins.replaceStrings [ "\n" ] [ "" ] (builtins.readFile path);
+
+  adminKeys = [
+    # Jet
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE40ISu3ydCqfdpb26JYD5cIN0Fu0id/FDS+xjB5zpqu jetthomaspham@gmail.com"
+  ];
+  mainWiki = readRecipient ./hosts/main-wiki.age.pub;
+  replicaWiki = readRecipient ./hosts/replica-wiki.age.pub;
+
+  allHosts = [
+    mainWiki
+    replicaWiki
+  ];
+in
+{
+  "shared/mysql-mediawiki.age".publicKeys = adminKeys ++ allHosts;
+  "shared/mysql-replication.age".publicKeys = adminKeys ++ allHosts;
+  "shared/mediawiki-admin-password.age".publicKeys = adminKeys ++ allHosts;
+  "shared/mediawiki-secret-key.age".publicKeys = adminKeys ++ allHosts;
+  "shared/mediawiki-upgrade-key.age".publicKeys = adminKeys ++ allHosts;
+}
